d2a4d729ccec6e65c2a7d2d17ce151cff5b766ba
NeyNey <drfrankenstein123@gmail.com>
2020-12-04 13:37:11 -0600

    i got a component wired up i feel like a million bucks

---
+++ emp-direct-app/src/App.js
@@ -0,0 +1,9 @@
+
+import './App.css';
+import MainContainer from "./components/maincontainer"
+
+function App() {
+  return <MainContainer />;
+}
+
+export default App;
---
+++ emp-direct-app/src/components/maincontainer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class MainContainer extends Component {
+    render(){
+        return (
+            <div className="App">
+              <header className="App-header">
+                <h1>nigg</h1>
+              </header>
+            </div>
+          );
+
+    }
+    
+  }
+
+  export default MainContainer;
