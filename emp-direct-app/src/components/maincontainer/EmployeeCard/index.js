b30cb78a76583f8a41c7d4050326fb88536a6119
NeyNey <drfrankenstein123@gmail.com>
2020-12-04 16:27:28 -0600

    got the name to display

--- emp-direct-app/src/components/maincontainer/EmployeeCard/index.js
+++ emp-direct-app/src/components/maincontainer/EmployeeCard/index.js
@@ -6,9 +6,9 @@ function EmployeeCard(props) {
             {props.results.map(result => (
                 <div className="card" style={{marginBottom: "1rem"}}>
                 <div className="card-body">
-                  <h4 className="card-title">{result.name}</h4>
-                  <p className="card-text"><strong>Minions Position: </strong>{result.position}</p>
+                  <h4 className="card-title">{result.name}</h4>                  
                   <p className="card-text"><strong>Minions Pay: </strong>{result.pay}</p>
+                  <p className="card-text"><strong>Minions Position: </strong>{result.position}</p>
                   <p className="card-text"><strong>Minions Department: </strong>{result.department}</p>
                   <p className="card-text"><strong>Minions Email: </strong>{result.email}</p>
                 </div>
--- emp-direct-app/src/components/maincontainer/index.js
+++ emp-direct-app/src/components/maincontainer/index.js
@@ -3,11 +3,24 @@ import Header from "./Header";
 import NameCard from "./namecard";
 import Name from "./name";
 import EmployeeCard from "./EmployeeCard"
+import Minion from "../../minions.json";
 
 class MainContainer extends Component {
     state ={
         results: []
     };
+    handleSingleEmployee = event => {
+        event.preventDefault();
+        const value = event.target.innerHTML;
+        let singleMinion = [];
+        for(var i =0; i<Minion.length; i++){
+            if(Minion[i].name === value){
+                singleMinion.push(Minion[i])
+            }
+        }
+        this.setState({results: singleMinion})
+    };
+    
     // componentDidMount() {
     //     this.handleViewAll()
     // }
@@ -20,7 +33,7 @@ class MainContainer extends Component {
                 <NameCard>
                 <div className="card-header" style={{textAlign: "center"}}>Click for View</div>
                 <div className="card-body" style={{textAlign: "center"}}>
-                <Name empName="wukawukaEMPLOYEENAMEGOESHEREwukawuka" class="btn btn-secondary"/>
+                {Minion.map(emp => ( <Name empName={emp.name} class="btn btn-secondary" handleSingleEmployee={this.handleSingleEmployee}/>))}
                 <Name empName="View All" class="btn btn-primary" />
                 <Name empName="Sort by Pay" class="btn btn-primary" />
                 <Name empName="View Dev Team" class="btn btn-primary"/>
