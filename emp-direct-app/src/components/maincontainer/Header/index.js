4427867dd626eda3052b079adb861903c04740d2
NeyNey <drfrankenstein123@gmail.com>
2020-12-05 19:53:34 -0600

    read me created

--- emp-direct-app/src/components/maincontainer/Header/index.js
+++ emp-direct-app/src/components/maincontainer/Header/index.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Header() {
   return (
-    <nav className="navbar navbar-dark bg-primary" style={{borderRadius: ".25rem", marginBottom: "1rem"}}>
+    
+    <nav className="navbar navbar-dark bg-primary" style={{borderRadius: ".25rem", marginBottom: "1rem" }}>
         <span className="navbar-brand mb-0 h1">Boss's (Minion) Directory</span>
     </nav>
+   
   );
 }
 
